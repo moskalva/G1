6bb3d022d104f5911e0300b08679118cc899231f
moskalva <vadym.mosalenko@outlook.com>
2024-05-30 06:50:28 +0200

    add server

---
+++ G1.Server/ClientConnect.cs
@@ -0,0 +1,39 @@
+using System.Net.WebSockets;
+
+namespace G1.Server;
+
+public class ClientConnect
+{
+    public static async Task Echo(WebSocket webSocket)
+    {
+        var buffer = new byte[1024 * 4];
+        var receiveResult = await webSocket.ReceiveAsync(
+            new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (!receiveResult.CloseStatus.HasValue)
+        {
+            var data = new ArraySegment<byte>(buffer, 0, receiveResult.Count);
+            Log(data);
+            await webSocket.SendAsync(
+                data,
+                receiveResult.MessageType,
+                receiveResult.EndOfMessage,
+                CancellationToken.None);
+            receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(
+            receiveResult.CloseStatus.Value,
+            receiveResult.CloseStatusDescription,
+            CancellationToken.None);
+    }
+
+    private static void Log(ArraySegment<byte> data)
+    {
+        var s = System.Text.Encoding.UTF8.GetString( data.ToArray());
+        Console.WriteLine("New message:\n========================");
+        Console.WriteLine(s);
+        Console.WriteLine("========================");
+    }
+}
---
+++ G1.Server/Program.cs
@@ -0,0 +1,36 @@
+using G1.Server;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var webSocketOptions = new WebSocketOptions
+{
+    KeepAliveInterval = TimeSpan.FromMinutes(2)
+};
+
+app.UseWebSockets(webSocketOptions);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/ws")
+    {
+        if (context.WebSockets.IsWebSocketRequest)
+        {
+            using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+            await ClientConnect.Echo(webSocket);
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+    }
+    else
+    {
+        await next(context);
+    }
+
+});
+
+app.Run();

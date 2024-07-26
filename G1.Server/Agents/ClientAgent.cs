d7999f3cf6d82d062581256e84d13f51d0ab3675
moskalva <vadym.mosalenko@outlook.com>
2024-07-26 09:21:44 +0200

    refactor storage state usage

--- G1.Server/Agents/ClientAgent.cs
+++ G1.Server/Agents/ClientAgent.cs
@@ -9,40 +9,46 @@ public class ClientAgent : Grain, IClientAgent
     private ObserverManager<IWorldEventsReceiver> worldEvents;
 
     private readonly IPersistentState<ClientAgentState> storage;
-    private ClientAgentState myState;
 
     public ClientAgent(
         ILogger<ClientAgent> logger,
         [PersistentState( stateName: "worldState", storageName: "agents")]
         IPersistentState<ClientAgentState> storage)
     {
-        this.myState = storage.RecordExists
-                     ? storage.State
-                     : new ClientAgentState
-                     {
-                         Id = this.GetPrimaryKey(),
-                         Position = ClientAgentState.Zero, // TODO: figure out initial position
-                         Velocity = AgentVelocity.Zero,
-                     }; 
-
         this.storage = storage;
         this.worldEvents = new ObserverManager<IWorldEventsReceiver>(TimeSpan.MaxValue, logger);
     }
 
-    public Task<ClientAgentState> GetState() => Task.FromResult(this.myState);
-
-    public Task Subscribe(IWorldEventsReceiver worldEvents)
+    public async Task<ClientAgentState> GetState()
     {
-        // notify subscriber with initial state
-        worldEvents.Notify(myState);
+        if(this.storage.RecordExists)
+            return this.storage.State;
 
-        this.worldEvents.Subscribe(worldEvents, worldEvents);
-        return Task.CompletedTask;
+        var newState = new ClientAgentState
+                     {
+                         Id = this.GetPrimaryKey(),
+                         Position = ClientAgentState.Zero, // TODO: figure out initial position
+                         Velocity = AgentVelocity.Zero,
+                     };
+        
+        await SaveState(newState);
+        return newState;
     }
 
-    public Task Unsubscribe(IWorldEventsReceiver worldEvents)
+    public async Task Subscribe(IWorldEventsReceiver receiver)
     {
-        this.worldEvents.Unsubscribe(worldEvents);
+        Console.WriteLine("Subscribed to events");
+        // notify subscriber with initial state
+        var currentState = await GetState();
+        await receiver.Notify(currentState);
+
+        this.worldEvents.Subscribe(receiver, receiver);
+    }
+
+    public Task Unsubscribe(IWorldEventsReceiver receiver)
+    {
+        Console.WriteLine("Unsubscribed from events");
+        this.worldEvents.Unsubscribe(receiver);
         return Task.CompletedTask;
     }
 
@@ -57,22 +63,21 @@ public class ClientAgent : Grain, IClientAgent
     {
         Console.WriteLine("New message:\n========================");
         Console.WriteLine(newState);
-        Console.WriteLine("========================");
 
-        if (newState.Id != myState.Id)
-            throw new InvalidOperationException($"State providded for wrong entity. This: '{myState.Id}' other: '{newState.Id}'");
+        var oldState = await GetState();
+        if (newState.Id != oldState.Id)
+            throw new InvalidOperationException($"State providded for wrong entity. This: '{oldState.Id}' other: '{newState.Id}'");
         
-        var oldState = this.myState;
-        this.myState = newState;
-        if (WorldPositionTools.TryNormalizePosition(myState.Position, out var normalizedPosition))
+        if (WorldPositionTools.TryNormalizePosition(newState.Position, out var normalizedPosition))
         {
-            this.myState.Position = normalizedPosition;
+            newState.Position = normalizedPosition;
             // TODO: notify base sector change
         }
 
         await SaveState(newState);
 
         await NotifyNearSectors(newState, oldState);
+        Console.WriteLine("========================");
     }
 
     private async Task NotifyNearSectors(ClientAgentState newState, ClientAgentState oldState)
@@ -88,14 +93,13 @@ public class ClientAgent : Grain, IClientAgent
         foreach (var oldSector in oldSectors)
         {
             var sectorAgent = this.GrainFactory.GetGrain<ISectorAgent>(oldSector.Raw);
-            await sectorAgent.Leave(this.myState.Id);
+            await sectorAgent.Leave(oldState.Id);
         }
     }
 
     private async Task SaveState(ClientAgentState newState)
     {
-        this.storage.State.Position = newState.Position;
-        this.storage.State.Velocity = newState.Velocity;
+        this.storage.State = newState;
 
         await storage.WriteStateAsync();
     }
